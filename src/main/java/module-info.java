6a1678d6bae2881a3e1a605890487174ead1a09c
santiagooff <santiago.olarteb@uqvirtual.edu.co>
2025-05-13 09:50:17 -0500

    Creacion del proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.bookyourstay {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.bookyourstay to javafx.fxml;
+    exports co.edu.uniquindio.bookyourstay;
+}
