6b96d5af08e5bcb7fed2ba8d3b78b26bcb966ed4
SantiagoAriasR <santiago.ariasr1@uqvirtual.edu.co>
2025-05-21 21:30:18 -0500

    Actualizar Panel Alojamientos

--- src/main/java/co/edu/uniquindio/bookyourstay/controladores/PanelAdministradorControlador.java
+++ src/main/java/co/edu/uniquindio/bookyourstay/controladores/PanelAdministradorControlador.java
@@ -2,6 +2,8 @@ package co.edu.uniquindio.bookyourstay.controladores;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 public class PanelAdministradorControlador {
     @FXML
@@ -14,7 +16,8 @@ public class PanelAdministradorControlador {
 
     @FXML
     void gestionarAlojamientos(ActionEvent event) {
-
+        Parent node = cargarPanel("GestionarAlojamientos.fxml");
+        panelPrincipal.getChildren().setAll(node);
     }
 
     @FXML
@@ -25,6 +28,18 @@ public class PanelAdministradorControlador {
     @FXML
     void obtenerEstadisticas(ActionEvent event) {
 
+    }
+
+    // Metodo para actualizar el panel
+    private Parent cargarPanel(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent node = loader.load();
+            return node;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
