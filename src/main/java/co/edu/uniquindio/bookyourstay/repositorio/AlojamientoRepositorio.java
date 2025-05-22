2725f0e64d0f62bad64cf1aff626058167911c19
SantiagoAriasR <santiago.ariasr1@uqvirtual.edu.co>
2025-05-21 23:55:45 -0500

    Filtros Alojamiento

--- src/main/java/co/edu/uniquindio/bookyourstay/repositorio/AlojamientoRepositorio.java
+++ src/main/java/co/edu/uniquindio/bookyourstay/repositorio/AlojamientoRepositorio.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.bookyourstay.repositorio;
 
+import co.edu.uniquindio.bookyourstay.modelo.enums.TipoAlojamiento;
 import co.edu.uniquindio.bookyourstay.modelo.factory.Alojamiento;
 
 import java.util.ArrayList;
@@ -41,5 +42,25 @@ public class AlojamientoRepositorio {
 
     public List<Alojamiento> listarAlojamientos() {
         return alojamientos;
+    }
+
+    public List<Alojamiento> buscarAlojamientoPorPrecio(float precioMin, float precioMax) {
+        List<Alojamiento> alojamientoFiltro = new ArrayList<>();
+        for (Alojamiento alojamiento : alojamientos) {
+            if(alojamiento.getPrecioPorNoche() < precioMax && alojamiento.getPrecioPorNoche() > precioMin) {
+                alojamientoFiltro.add(alojamiento);
+            }
+        }
+        return alojamientoFiltro;
+    }
+
+    public List<Alojamiento> buscarAlojamientoPorTipo(TipoAlojamiento tipoAlojamiento){
+        List<Alojamiento> alojamientoFiltro = new ArrayList<>();
+        for (Alojamiento alojamiento : alojamientos) {
+            if (alojamiento.getTipoAlojamiento() == tipoAlojamiento){
+                alojamientoFiltro.add(alojamiento);
+            }
+        }
+        return alojamientoFiltro;
     }
 }
